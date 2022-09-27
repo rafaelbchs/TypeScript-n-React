ce646680d95dd5f1aed1d16ab70344b674ea1918
rafaelbchs <rafaelbchacins@gmail.com>
2022-09-27 13:54:41 -0500

    added state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.models';
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+
+  const todoAddHandler = (text:string) => {
+    setTodos(prevTodos => [
+      ...prevTodos, 
+      {id: Math.random().toString(), text: text}
+    ]);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos} />
     </div>
   );
